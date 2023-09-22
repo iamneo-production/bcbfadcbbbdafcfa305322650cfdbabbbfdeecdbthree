08f51eb9d0d302d77ac7248f69658f4ed5272c46
Iamneo <techadmin@iamneo.ai>
2023-09-22 11:30:02 +0000

    Backup Commit

--- ecinstance/rahul.tf
+++ ecinstance/rahul.tf
@@ -1,7 +1,19 @@
 terraform {
   required_providers {
       aws = {
-          
-      }
-  }
+          source = "hashicorp/aws"
+        }
+    }
+}
+provider "aws" {
+    region = "ap-northeast-1"
+    access_key = "AKIA2SACZ4LTDR4WR6WI"
+    secret_key = "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"
+}
+resource "aws_instance" "exam" {
+    ami = "ami-0ed99df77a82560e6"
+    instance_type = "t2.micro"
+}
+output "public_ip"{
+    value = aws_instance.exam.public_ip
 }
